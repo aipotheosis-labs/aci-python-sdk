70e797c9c236f18f070ca13ed159321d4b4aed12
fixer <14433035+thisisfixer@users.noreply.github.com>
2025-02-26 12:15:04 +0000

    remove App and Function enums; use string directly instead

--- aipolabs/types/apps.py
+++ aipolabs/types/apps.py
@@ -1,5 +1,3 @@
-from typing import ClassVar
-
 from pydantic import BaseModel
 
 from aipolabs.types.functions import Function
@@ -20,23 +18,11 @@ class SearchAppsParams(BaseModel):
 
 
 class App(BaseModel):
-    """Representation of an application. Search results will return a list of these.
-    Also provides enum-like functionality for easily accessing supported applications.
-
-    Please note that we might add new Apps in our backend dynamically, so the constant values might
-    not be exhaustive.
-    You can either upgrade your SDK to the latest version or just use string values
-    (e.g. "BRAVE_SEARCH__WEB_SEARCH", check out docs for most up-to-date list of supported apps)
-    for App related operations.
-    """
+    """Representation of an application. Search results will return a list of these."""
 
     # instance attributes should match the schema defined on the server side.
     name: str
     description: str
-
-    # Class-level constants for supported apps
-    # TODO: add all supported apps
-    BRAVE_SEARCH__WEB_SEARCH: ClassVar[str] = "BRAVE_SEARCH__WEB_SEARCH"
 
 
 class AppDetails(App):
